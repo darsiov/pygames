7d39e15fe0b261425812c190614260c8b526d526
darsiov <arturoviedo0630@gmail.com>
2022-01-21 14:10:40 -0500

    Arreglado de bugs, animación del futuro fondo arreglada

--- first_game.py
+++ first_game.py
@@ -22,19 +22,22 @@ def main():
             self.rect = self.image.get_rect()
             self.rect.center = (WIDTH/2, HEIGTH/2)
 
-            self.frames = 4 
-            self.current_frame = 0
-            self.frame_width = 64
-            self.frame_heigth = 64
+            self.frames = 4 #Limite de frames
+            self.current_frame = 0 #Frame actual
+            self.frame_width = 64   #Ancho del frame
+            self.frame_heigth = 64  #Alto del frame
 
 
         def update(self, dt, window):
-            if self.current_frame >= self.frames-1:
+            if dt >= 0.35:
+                dt = 0.33
+            if self.current_frame >= self.frames - 1:
                 self.current_frame = 0 
             else:
-                self.current_frame += 3*dt
-            print(int(self.current_frame)*self.frame_width)
-            self.image = pygame.transform.scale(self.spriteSheet.subsurface((int(self.current_frame)*self.frame_width,0,64,64)),(240,240))
+                self.current_frame += int(3*dt)
+                print(str(self.current_frame)+' Frame actual del sprite')
+
+            self.image = pygame.transform.scale(self.spriteSheet.subsurface((int(self.current_frame)*self.frame_width,0,64,64)),(400,400))
     
 
     wallpaper = Fondo()
@@ -43,18 +46,21 @@ def main():
 
     while True:
 
-        dt = clock.tick(30) /100
+        dt = clock.tick(30) /100 #Hz
+        window.fill((0, 0, 0))
+        group_sprites.update(dt, window)
+        group_sprites.draw(window)
+        print(str(dt) + ' Frames' )
+        pygame.display.flip()
 
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()   
+                
 
-        window.fill((0, 0, 0))
-        group_sprites.update(dt, window)
-        group_sprites.draw(window)
-        pygame.display.flip() 
+        
 
      
     
